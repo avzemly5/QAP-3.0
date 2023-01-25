5a32f14b2a1135a558c8fe8de79c02979704830b
avzemly5 <ssashazem@gmail.com>
2023-01-26 01:18:40 +0300

    Задание 21.8.2

---
+++ python_practice/rectangle_2.py
@@ -0,0 +1,23 @@
+from rectangle_1 import Rectangle, Square, Circle
+#далее создаем два прямоугольника
+rect_1 = Rectangle(3,4)
+rect_2 = Rectangle(12,5)
+# вывод площади наших двух прямоугольников
+print(rect_1.get_area())
+print(rect_2.get_area())
+
+square_1 = Square(5)
+square_2 = Square(10)
+
+print(square_1.get_area_aquare(),
+      square_2.get_area_aquare())
+
+figures = [rect_1, rect_2, square_1, square_2]
+for figure in figures:
+    if isinstance(figure,Square):
+        print(figure.get_area_aquare())
+    else:
+        print(figure.get_area())
+
+circle_1 = Circle(5)
+print(circle_1.get_area_circle())
